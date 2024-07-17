25093766d3f4c61276ff05668b6bd8e1a6e0dfa7
zjw <20307130189@fudan.edu.cn>
2024-07-18 02:33:37 +0800

    fix: fix bugs with version of pl for spbnet.visualize.feat

--- tests/feat.py
+++ tests/feat.py
@@ -5,6 +5,6 @@ sys.path.append("..")  # 相对路径或绝对路径
 from spbnet.visualize.feat import feat
 import os
 
-# os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+os.environ["CUDA_VISIBLE_DEVICES"] = "7"
 
 feat("./config.feat.yaml")
